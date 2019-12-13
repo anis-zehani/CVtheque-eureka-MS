7f4598919baa9607c6a854546a98c899def30d38
GitLabOdix <azaheni@smartgraphe.com>
2019-12-13 20:37:19 +0100

    first commit

---
+++ src/test/java/com/eureka/org/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.eureka.org;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
